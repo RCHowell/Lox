90194ed5e80a891ac7916cf94a9b81c96dd25428
R. C. Howell <rchowell19@gmail.com>
2023-03-03 20:20:29 -0800

    [lox-c] Chapter 26

--- lox-c/memory.h
+++ lox-c/memory.h
@@ -20,6 +20,12 @@
 
 void* reallocate(void* pointer, size_t oldSize, size_t newSize);
 
+void markValue(Value value);
+
+void markObject(Obj* object);
+
+void collectGarbage();
+
 void freeObjects();
 
 #endif
--- lox-c/object.c
+++ lox-c/object.c
@@ -13,8 +13,12 @@
 static Obj* allocateObject(size_t size, ObjType type) {
     Obj* object = (Obj*) reallocate(NULL, 0, size);
     object->type = type;
+    object->isMarked = false;
     object->next = vm.objects;
     vm.objects = object;
+#ifdef DEBUG_LOG_GC
+    printf("%p allocate %zu for %d\n", (void*) object, size, type);
+#endif
     return object;
 }
 
@@ -53,7 +57,11 @@ static ObjString* allocateString(char* chars, int length, uint32_t hash) {
     objString->length = length;
     objString->chars = chars;
     objString->hash = hash;
+
+    push(OBJ_VAL(objString));
     tableSet(&vm.strings, objString, NIL_VAL);
+    pop();
+
     return objString;
 }
 
